658243ee34cd647d4db3796a573563e74a4be0e8
keisuke ishizuka <keisuke.ishizuka@rakus-partners.co.jp>
2022-08-03 09:30:22 +0900

    administratorRepositoryのinsertメソッドの追加

--- src/main/java/jp/co/sample/service/AddministratorService.java
+++ src/main/java/jp/co/sample/service/AddministratorService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.repository.AdministratorRepository;
+
 
 @Service
 @Transactional
@@ -12,4 +14,12 @@ public class AddministratorService {
 
 	@Autowired
 	private AdministratorRepository administratorRepository;
+	
+	/**
+	 * 管理者情報の挿入
+	 *
+	 */
+	public void insert(Administrator administrator) {
+		administratorRepository.insert(administrator);
+	}
 }
