136fc207b8c9b98c553dfc3eab8caa654d29d007
keisuke ishizuka <keisuke.ishizuka@rakus-partners.co.jp>
2022-08-03 15:17:10 +0900

    Controllerクラスへのメソッドの追加及びdetail.htmlの作成

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Employee;
+import jp.co.sample.form.UpdateEmployeeForm;
 import jp.co.sample.service.EmployeeService;
 
 /**
@@ -31,6 +33,24 @@ public class EmployeeController {
 		model.addAttribute("employeeList", employeeList);
 		
 		return "employee/list.html";
+	}
+	
+	/**
+	 * UpdateEmployeeFormをインスタンス化
+	 *
+	 */
+	@ModelAttribute
+	public UpdateEmployeeForm setUpUpdateEmployeeForm() {
+		UpdateEmployeeForm updateEmployeeForm = new UpdateEmployeeForm();
+		return updateEmployeeForm;
+	}
+	
+	public String showDetail(String id, Model model) {
+		int empId = Integer.parseInt(id);
+		Employee employee
+			= employeeService.showDetail(empId);
+		model.addAttribute("employee",employee);
+		return "employee/detail.html";
 		
 	}
 }
